0eaecf0bdee03fa1ccde685c62b92bc3d05a7861
Brent <puddletowndesign@gmail.com>
2017-04-01 17:48:22 -0600

    updated installer preferences

--- install.sh
+++ install.sh
@@ -14,16 +14,11 @@ git clone https://github.com/PuddletownDesign/Brew
 ./Brew/install.sh
 cd ~/Documents/Dev/
 
-echo "downloading zsh configuration"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-
-echo "Cloning Configs"
-
-
+echo "Setting up iterm"
 git clone https://github.com/PuddletownDesign/iTerm
 cd ~/Documents/Dev/iTerm
-git pull orgin mac
+git pull origin mac
 git checkout mac
 ./iTerm/install.sh
 cd ~/Documents/Dev/
@@ -32,21 +27,24 @@ cd ~/Documents/Dev/
 echo "Installing Atom"
 git clone https://github.com/PuddletownDesign/Atom
 cd ~/Documents/Dev/Atom
-git pull orgin mac
+git pull origin mac
 git checkout mac
 ./Atom/install.sh
+cd ~/Documents/Dev/
+
+echo "Installing zshrc and theme...."
+git clone https://github.com/PuddletownDesign/ZSH
+cd ~/Documents/Dev/ZSH
+git pull origin mac
+git checkout mac
+./ZSH/install.sh
 cd ~/Documents/Dev/
 
 echo "downloading oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "Installing zshrc and theme...."
-git clone https://github.com/PuddletownDesign/ZSH
-cd ~/Documents/Dev/ZSH
-git pull orgin mac
-git checkout mac
-./ZSH/install.sh
-cd ~/Documents/Dev/
+echo "downloading zsh configuration"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo "setting shell as zsh"
 sudo chsh -s /usr/bin/zsh
