c088511adf05804e8f6dc251e8f759fff2e8d2e4
Brent <puddletowndesign@gmail.com>
2017-04-02 14:56:48 -0500

    updated linux

--- install.sh
+++ install.sh
@@ -9,39 +9,46 @@ sudo apt-get update
 
 echo "downloading apt-get packages"
 # try to do this from a generated list in the brew folder
-sudo apt-get install -y software-properties-common zsh curl tree git
-sudo apt-add-repository ppa:webupd8team/atom
-sudo apt-get update
+sudo apt-get install software-properties-common fonts-inconsolata zsh curl tree git python3-dev python-pip python3-pip  -y
+pip install --upgrade pip
+pip3 install --user thefuck
 
-
-sudo apt-get install atom
 
 
 echo "downloading oh-my-zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 echo "downloading zsh configuration"
+git clone https://github.com/PuddletownDesign/ZSH
+cd ~/Config/ZSH
+
+./install.sh
+cd ~/Config/
+
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 
-
-git clone https://github.com/PuddletownDesign/Git
-git clone https://github.com/PuddletownDesign/Atom
-git clone https://github.com/PuddletownDesign/ZSH
-
 echo "Installing Git Configs"
-./Git/install.sh
+git clone https://github.com/PuddletownDesign/Git
 
+cd ~/Config/Git
+./install.sh
+
+mkdir ~/Config/temp
+cd ~/Config/temp/
 
 # check to see if there are any needed casks for install
-
 echo "Installing Atom"
+wget https://atom.io/download/deb -O atom.deb
+sudo dpkg -i atom.deb
+
+cd ~/Config/
+git clone https://github.com/PuddletownDesign/Atom
 cd ~/Config/Atom
-git reset --hard origin/linux
-git checkout linux
 ./Atom/install.sh
 cd ~/Config/
 
 #download zsh and zsh theme
 echo "setting shell as zsh"
-chsh -s /usr/bin/zsh
+chsh -s $(which zsh)
