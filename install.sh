e03b12e3ecc1c55736b10299096c8c4d6dff3068
Brent <puddletowndesign@gmail.com>
2017-04-17 23:45:22 -0500

    added headless linux variant without atom and other gui tools

--- install.sh
+++ install.sh
@@ -9,10 +9,8 @@ sudo apt-get update
 
 echo "downloading apt-get packages"
 # try to do this from a generated list in the brew folder
-sudo apt-get install software-properties-common fonts-inconsolata zsh curl tree git python3-dev python-pip python3-pip  -y
-pip install --upgrade pip
+sudo apt-get install software-properties-common fonts-inconsolata zsh curl tree python3-dev python-pip python3-pip  -y
 pip3 install --user thefuck
-
 
 
 echo "downloading oh-my-zsh"
@@ -21,34 +19,20 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 echo "downloading zsh configuration"
 git clone https://github.com/PuddletownDesign/ZSH
 cd ~/Config/ZSH
+git checkout linux
 
 ./install.sh
 cd ~/Config/
-
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-
 echo "Installing Git Configs"
 git clone https://github.com/PuddletownDesign/Git
-
 cd ~/Config/Git
+git checkout linux
 ./install.sh
-
-mkdir ~/Config/temp
-cd ~/Config/temp/
-
-# check to see if there are any needed casks for install
-echo "Installing Atom"
-wget https://atom.io/download/deb -O atom.deb
-sudo dpkg -i atom.deb
-
-cd ~/Config/
-git clone https://github.com/PuddletownDesign/Atom
-cd ~/Config/Atom
-./Atom/install.sh
-cd ~/Config/
 
 #download zsh and zsh theme
 echo "setting shell as zsh"
-chsh -s $(which zsh)
+cd ~
+zsh
